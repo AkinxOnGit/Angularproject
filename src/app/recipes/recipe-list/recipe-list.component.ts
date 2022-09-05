5c4a16ddafb6c3bc7dacccc3e311454eb8de6cf0
Mustafa Akin <mustafa.akin@twinformatics.at>
2022-09-05 15:09:09 +0200

    EP-57: Working on the ShoppingListComponent

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,7 @@ export class RecipeListComponent implements OnInit {
     new Recipe("Lasagne", "This is a Lasagne recipe", "https://static.sscontent.com/thumb/500/500/products/124/v998629_prozis_tuna-lasagna_6.jpg"),
     new Recipe("Lasagne", "This is a Lasagne recipe", "https://static.sscontent.com/thumb/500/500/products/124/v998629_prozis_tuna-lasagna_6.jpg")
   ];
+  status: boolean = true
 
   constructor() { }
 
